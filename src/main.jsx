ee01a6bb829954a90bc9977652f147622ab852c1
jyotisolanki1998 <solanki.jyoti@webseeder.tech>
2025-08-02 20:10:36 +0530

    update the file

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App'
 import { Provider } from 'react-redux'
 import { store } from './emailStore';
 import EmailCampaign from './EmailCampaign'
