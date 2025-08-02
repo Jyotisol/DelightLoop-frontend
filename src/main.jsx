3359d3f2d1a3af996e54a48e5f504e7d16168628
jyotisolanki1998 <solanki.jyoti@webseeder.tech>
2025-08-02 19:50:00 +0530

    removed unused file

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { Provider } from 'react-redux'
-// import { store } from './store'
 import { store } from './emailStore';
 import EmailCampaign from './EmailCampaign'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    <Provider store={store}>
-   {/* <App /> */}
    <EmailCampaign />
    </Provider>
   </StrictMode>,
